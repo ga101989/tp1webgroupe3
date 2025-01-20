887c71bf8a8e56a7b4abe2fb65229bdc6b9b20c2
ga101989 <axel.guillou@etu.unice.fr>
2025-01-20 12:12:10 +0100

    articles

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Javascript lié avec succés !');
+
+const btnAjtAuPanier = document.getElementById('btn1');
+const btnAjtAuPanier2 = document.getElementById('btn2');
+const btnAjtAuPanier3 = document.getElementById('btn3');
+console.log(btnAjtAuPanier);
+console.log(btnAjtAuPanier2);
+console.log(btnAjtAuPanier3);
+
+btnAjtAuPanier.addEventListener('click', function() {
+    alert('Harry Potter 1 ajouté au panier !');
+});
+
+btnAjtAuPanier2.addEventListener('click', function() {
+    alert('Harry Potter 2 ajouté au panier !');
+});
+
+btnAjtAuPanier3.addEventListener('click', function() {  
+    alert('Harry Potter 3 ajouté au panier !');
+});
